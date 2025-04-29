95d57a1aee07dd15481d625544cb71afd00852ca
Ananya Sehgal <ananyasehgal@Ananyas-MacBook-Air-148.local>
2025-04-28 19:22:56 -0500

    Created module for creating chromadb and uploading video transcripts to it

---
+++ vector-db.py
@@ -0,0 +1,50 @@
+import chromadb
+import uuid
+
+class ChromaDB:
+    def __init__(self):
+        self.collection = None
+        self.embedding_model = None
+
+    def initialize_chroma_db_collection(self, collection_name: str) -> chromadb.Collection:
+        chroma_client = chromadb.Client()
+        if self.embedding_model != None:
+            self.collection = chroma_client.get_or_create_collection(name=collection_name, embedding_function=self.embedding_model)
+        else:
+            self.collection = chroma_client.get_or_create_collection(name=collection_name)
+
+    def upload_video_to_collection(self, video_name: str, video_transcript: str, start_time: int, end_time: int, id=str(uuid.uuid5()), embedding_vector=None) -> None:
+        try:
+            if embedding_vector != None:
+                self.collection.add(
+                    ids=[id],
+                    embeddings= [embedding_vector],
+                    documents=[video_transcript],
+                    metadatas=[{"video-source":video_name, "start-time":start_time, "end-time":end_time}]
+                )
+            else:
+                 self.collection.add(
+                    ids=[id],
+                    documents=[video_transcript],
+                    metadatas=[{"video-source":video_name, "start-time":start_time, "end-time":end_time}]
+                )
+        except Exception as e:
+            print("Error uploading video to collection:", e)
+        else:
+            print("Successfully added video to collection!")
+    
+    def upload_chunks_to_collection(self, video_name: str, video_transcripts=[], times = [], ids=[], auto_embed_videos=False):
+        if len(times) != len(video_transcripts):
+            print("Each video chunk must have a transcript/description and start/end times!")
+            return
+        if len(ids) != 0 and len(ids) != len(video_transcripts):
+            print("If not using default ids, each video chunk needs to have a unique id")
+        
+        for i in range(len(video_transcripts)):
+            video_transcript = video_transcripts[i]
+            start_time, end_time = times[i]
+            self.upload_video_to_collection(video_name, video_transcript, start_time, end_time)
+    
+            
+    
+    
