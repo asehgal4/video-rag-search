771bcfdd2824efa8d46facfa74e1927d62d7ac64
Ananya Sehgal <ananyasehgal@Ananyas-MacBook-Air-148.local>
2025-04-29 20:49:16 -0500

    seperated existing code into a initialize-db stage of the pipeline. Will need a new folder/stage for the query part once the database has been created

---
+++ initialize-db/initialize_db.py
@@ -0,0 +1,31 @@
+from caption_video import caption_video
+from vector_db_utils import ChromaDB
+from video_slicer import chunk_video
+import argparse
+import os
+
+
+def initialize_db(video_paths: list, video_names: list, chunk_times: list):
+    db = ChromaDB()
+    db.initialize_chroma_db_collection("videos")
+
+    for video_path, video_name, chunk_time in zip(video_paths, video_names, chunk_times):
+        chunk_folder = chunk_video(video_path, video_name, chunk_time)
+        video_descriptions, start_end_times = caption_video(chunk_folder)
+
+        video_chunk_names = sorted(os.listdir(chunk_folder))
+        db.upload_chunks_to_collection(video_name, video_chunk_names, video_descriptions, start_end_times)
+    
+    return db
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This initializes the vector database with videos and their chunks for the rag pipeline")
+    parser.add_argument('filepath', help='Path to input mp4 file')
+    parser.add_argument('video_name', help='Title of the video')
+    parser.add_argument('--chunk_time', '-t', help='Optional specified time span of each chunk', default=10)
+    args = parser.parse_args()
+
+    video_paths, video_names, chunk_times = [args.filepath], [args.video_name], [int(args.chunk_time)]
+    initialize_db(video_paths, video_names, chunk_times)
